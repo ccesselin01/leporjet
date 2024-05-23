bf8790b839d8bd6e35a4b0e9195a668b0367d3c3
cesse <cesse@DESKTOP-S4CF9A1>
2024-05-23 18:15:50 +0200

    Merge origin/master svp fonctionne Conflicts: 	src/main/java/fr/insa/porjet/leporjet/Etage.java

--- projet2024/src/main/java/com/insa/ccesselin01/calcul/Appartement.java
+++ projet2024/src/main/java/com/insa/ccesselin01/calcul/Appartement.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.insa.ccesselin01.calcul;
-
+//hello
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
